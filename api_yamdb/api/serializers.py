b740a60b4975cdf50ecec057c034a3d6123c44ac
Вячеслав Петрукович <petrukovich.viachaslau@gmail.com>
2022-08-16 01:33:03 +0300

    добавлена проверка на уникальность email при регистрации

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,10 +1,16 @@
-from rest_framework import serializers
-
 from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True, allow_blank=False)
+    email = serializers.EmailField(required=True,
+                                   allow_blank=False,
+                                   validators=[
+                                       UniqueValidator(
+                                           queryset=User.objects.all(),
+                                           message='Пользователь с таким '
+                                                   'email уже существует!')])
 
     class Meta:
         model = User
