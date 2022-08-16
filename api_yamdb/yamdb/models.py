e0a26f8a9bffb64ca76d32cbce78e3a7217c4402
Александр Лыо <al.lyo@yandex.ru>
2022-08-16 17:20:34 +0300

    Созданы модели Categories, Genres, Titles

--- api_yamdb/yamdb/models.py
+++ api_yamdb/yamdb/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genres,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='titles'
+    )
+    category = models.ForeignKey(
+        Categories,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='titles'
+    )
+
+    def __str__(self):
+        return self.name
