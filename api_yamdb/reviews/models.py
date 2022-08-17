86d706bf4f1fbbaa5954ef7f4c16e6021592dc48
Александр Лыо <al.lyo@yandex.ru>
2022-08-17 19:15:32 +0300

    Исправлены модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,29 +2,29 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField()
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Genres(models.Model):
-    name = models.CharField()
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Titles(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
     description = models.TextField()
-    genre = models.ForeignKey(
+    genre = models.ManyToManyField(
         Genres,
         blank=True,
         null=True,
-        on_delete=models.SET_NULL,
         related_name='titles'
     )
     category = models.ForeignKey(
