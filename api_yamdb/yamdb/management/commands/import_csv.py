3f04ba9596674813cb22c7674787195b272aefec
Кульчинский Эдуард <eduardbit@yandex.ru>
2022-08-17 23:39:54 +0300

    search for files in the directory

--- api_yamdb/yamdb/management/commands/import_csv.py
+++ api_yamdb/yamdb/management/commands/import_csv.py
@@ -1,6 +1,10 @@
-from django.core.management import BaseCommand
+from os import listdir
+from os.path import exists, isfile, join, splitext
+from pathlib import Path
+
+from django.apps import apps
 from django.conf import settings
-import os
+from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
@@ -14,10 +18,20 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        self.stdout.write('Тестовая команда')
-        path = os.path.join(
+        print('Тестовая команда')
+        input_path = Path(options['path'])
+        path = join(
             settings.BASE_DIR,
-            options['path']
+            input_path
         )
-        if os.path.exists(path)
+        self.stdout.write('Введена директория:')
         self.stdout.write(str(path))
+        if exists(path):
+            self.stdout.write('Директория найдена...')
+        else:
+            raise FileNotFoundError('Директория НЕ найдена!')
+        self.stdout.write('Поиск файлов по названию таблиц...')
+        # apps.all_models['']
+        files_list = [f for f in listdir(path) if isfile(join(path, f))]
+        print(files_list)
+        
