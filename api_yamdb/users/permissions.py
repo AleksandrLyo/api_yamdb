6bbb6c1118dea2fb8cc1b0f2df9e328d0745012d
Petrukovich <cram13@tut.by>
2022-08-19 09:04:32 +0300

    Исправлен возможный баг с superuser

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -8,18 +8,20 @@ class IsAuthorStaffOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user.role == 'moderator':
             return request.method == 'DELETE'
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user or request.user.role == 'admin')
+        return (request.method in permissions.SAFE_METHODS or
+                obj.author == request.user or
+                request.user.role == 'admin' or
+                request.user.is_superuser)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS or
-                request.user.role == 'admin' or request.user.is_staff is True)
+                request.user.role == 'admin' or request.user.is_superuser)
 
 
 class IsAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return (request.user.role == 'admin' or
-                    request.user.is_superuser is True)
+                    request.user.is_superuser)
